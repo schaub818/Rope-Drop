e1c6e24b5857996e531df3d67fc40828dad92086
schaub818 <schaub818@gmail.com>
2023-01-21 12:50:19 -0500

    Started implementing Timeline code

---
+++ RopeDrop/Assets/Scripts/Timeline.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RopeDropGame
+{
+    public class Timeline : MonoBehaviour
+    {
+        public DateTime RopeDrop
+        {
+            get { return ropeDrop; }
+        }
+
+        public DateTime ParkClose
+        {
+            get { return parkClose; }
+        }
+
+        public DateTime CurrentTime
+        {
+            get { return timeChunks[currentTimeIndex].Time; }
+        }
+
+        private DateTime ropeDrop;
+
+        [SerializeField]
+        private DateTime parkClose;
+
+        [SerializeField]
+        private int timeChunkSize = 5;
+
+        private int currentTimeIndex = 0;
+
+        private List<TimelineChunk> timeChunks;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ropeDrop = new DateTime(2023, 1, 21, 7, 0, 0);
+            parkClose = new DateTime(2023, 1, 21, 9, 0, 0);
+            timeChunks = new List<TimelineChunk>();
+
+            TimeSpan dayLength = parkClose - ropeDrop;
+
+            int numTimeChunks = (int)dayLength.TotalMinutes / timeChunkSize + 1;
+
+            for (int i = 0; i < numTimeChunks; i++)
+            {
+                TimeSpan minutesAdd = new TimeSpan(0, i * timeChunkSize, 0);
+
+                timeChunks.Add(new TimelineChunk(ropeDrop.Add(minutesAdd)));
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public TimelineChunk GetFutureTime(int numChunksForward)
+        {
+            if (currentTimeIndex + numChunksForward < timeChunks.Count - 1)
+            {
+                return timeChunks[currentTimeIndex + numChunksForward];
+            }
+            else
+            {
+                Debug.LogError("Trying to get future time past park close");
+
+                return timeChunks[currentTimeIndex];
+            }
+        }
+
+        public bool IsFutureTimePastParkClose(int numChunksForward)
+        {
+            if (currentTimeIndex + numChunksForward < timeChunks.Count - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AdvanceTime(int numChunksForward)
+        {
+            if (currentTimeIndex + numChunksForward < timeChunks.Count - 1)
+            {
+                currentTimeIndex += numChunksForward;
+
+                return true;
+            }
+            else
+            {
+                Debug.LogError("Trying to advance time past park close");
+
+                return false;
+            }
+        }
+    }
+}
---
+++ RopeDrop/Assets/Scripts/TimelineChunk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RopeDropGame
+{
+    public struct TimelineChunk
+    {
+        public TimelineChunk(DateTime time)
+        {
+            Time = time;
+        }
+
+        public DateTime Time
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return Time.ToShortTimeString();
+        }
+    }
+}
