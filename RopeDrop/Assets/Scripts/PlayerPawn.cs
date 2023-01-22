7048bdd96aa64b4f1ed818e351ebee682350b79e
Dave Schaub <schaub818@gmail.com>
2023-01-21 20:10:29 -0500

    Implemented Pawn code.

---
+++ RopeDrop/Assets/Scripts/PlayerPawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RopeDropGame
+{
+    public class PlayerPawn : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Move(MapLocation location)
+        {
+            transform.position = location.Position;
+        }
+    }
+}
